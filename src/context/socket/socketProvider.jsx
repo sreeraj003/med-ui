c62f20ca493d7f556c492282ccf37bd4131e3d83
Sreeraj K <sreerajkattippallam003@gmail.com>
2023-07-03 19:31:01 +0530

    socket

--- src/context/socket/socketProvider.jsx
+++ src/context/socket/socketProvider.jsx
@@ -11,7 +11,7 @@ export const useSocket = () => {
 
 export const SocketProvider = (props) => {
 
-    const socket = useMemo(() => io('localhost:8080'), [])
+    const socket = useMemo(() => io('https://medcoreonline.netlify.app'), [])
     return (
         <SocketContext.Provider value={socket} >
             {props.children}
