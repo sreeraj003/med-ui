08fbea366c97dbc5ec699776348471562f86d804
Sreeraj K <sreerajkattippallam003@gmail.com>
2023-07-10 12:14:34 +0530

    payment update

--- src/components/userComponents/payment.jsx
+++ src/components/userComponents/payment.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './appointments/appointment.css'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
@@ -8,7 +8,8 @@ function Payment() {
 
     const history = useNavigate()
 
-    // const userToken = localStorage.getItem('userToken')
+    const [err, setErr] = useState()
+    const userToken = localStorage.getItem('userToken')
     const appData = useSelector(state => state.appointment.appointment)
     const amount = appData.fee + (appData.fee * 0.1)
 
@@ -20,23 +21,30 @@ function Payment() {
     const handlePay = async () => {
         try {
 
-            const payment = await displayRazorpay(amount);
-            if (payment) {
-                console.log(appData);
-                await axios.post(import.meta.env.VITE_BASE_URL + `bookslot`, appData, {
-                    // headers: {
-                    //     Authorization: `Bearer ${userToken}`
-                    // }
-                }).then(res => {
-                    if (res.data == 'success') {
-                        history('/success')
-                    } else if (res.data == 'blocked') {
-                        history('/login')
-                        localStorage.removeItem('userToken')
-                    }
-                })
+            const payment = await displayRazorpay(amount, appData);
+            if (payment == 'slot unavailable') {
+                setErr(payment)
+                setTimeout(() => {
+                    setErr('')
+                }, 4000)
             } else {
-                console.log('payment error');
+                if (payment) {
+                    console.log(appData);
+                    await axios.post(import.meta.env.VITE_BASE_URL + `bookslot`, appData, {
+                        headers: {
+                            Authorization: `Bearer ${userToken}`
+                        }
+                    }).then(res => {
+                        if (res.data == 'success') {
+                            history('/success')
+                        } else if (res.data == 'blocked') {
+                            history('/login')
+                            localStorage.removeItem('userToken')
+                        }
+                    })
+                } else {
+                    console.log('payment error');
+                }
             }
         } catch (error) {
             console.log(error);
@@ -45,6 +53,7 @@ function Payment() {
 
     return (
         <div className="slice m-3 mx-auto mt-5 mb-5 p-2 app-div" style={{ maxWidth: '1200px' }}>
+            {err ? <div className="alert alert-danger text-center">{err}</div> : ''}
             <div className="container">
                 <div className="row">
                     <div className="col-lg-6">
